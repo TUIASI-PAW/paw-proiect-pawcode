817d054bb7e59253e1a556f5357304a3796abebb
ana0998 <ana-gabriela.flocea@student.tuiasi.ro>
2020-11-15 14:34:35 +0200

    Realizare legatura dintre / si modulul oferte

--- RoTravel/RoTravel/src/app/app-routing.module.ts
+++ RoTravel/RoTravel/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'oferte',
+    redirectTo:'oferte',
+    pathMatch:'full'
+  },
+  {
+    path:'oferte',
+    loadChildren:() => import('./oferte/oferte.module').then(mod => mod.OferteModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
