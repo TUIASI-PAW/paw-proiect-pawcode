1a743abb0b14720015f61c8b1c9e4af53ee3532f
ana0998 <ana-gabriela.flocea@student.tuiasi.ro>
2020-11-15 14:45:14 +0200

    Adaugarea logicii de afisare a persoanelor

--- RoTravel/RoTravel/src/app/oferte/gestionare-oferte/gestionare-oferte.component.ts
+++ RoTravel/RoTravel/src/app/oferte/gestionare-oferte/gestionare-oferte.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs'
+import {OferteService} from '../oferte.service'
+import {Oferta} from '../models'
+
 
 @Component({
   selector: 'app-gestionare-oferte',
   templateUrl: './gestionare-oferte.component.html',
   styleUrls: ['./gestionare-oferte.component.css']
 })
-export class GestionareOferteComponent implements OnInit {
 
-  constructor() { }
+
+export class GestionareOferteComponent implements OnInit,OnDestroy {
+
+  private subscription:Subscription;
+  oferte:Oferta[];
+
+  constructor(private oferteService:OferteService) { }
 
   ngOnInit(): void {
+    this.getOferte();
+  }
+
+  ngOnDestroy():void{
+    this.subscription.unsubscribe();
+  }
+
+  private getOferte(){
+    this.subscription = this.oferteService.get().subscribe((oferte:Oferta[])=>{
+      this.oferte = oferte;
+    })
+
   }
 
 }
