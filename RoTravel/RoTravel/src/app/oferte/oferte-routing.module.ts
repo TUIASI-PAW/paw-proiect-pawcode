7a2c413bdd94041463923cfe37e8c1e80e0458c9
ana0998 <ana-gabriela.flocea@student.tuiasi.ro>
2020-11-15 14:38:35 +0200

    Configurare cale catre afisarea html -ului din gestionare-oferte.component.html

--- RoTravel/RoTravel/src/app/oferte/oferte-routing.module.ts
+++ RoTravel/RoTravel/src/app/oferte/oferte-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GestionareOferteComponent} from './gestionare-oferte/gestionare-oferte.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component:GestionareOferteComponent,
+    pathMatch:'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
