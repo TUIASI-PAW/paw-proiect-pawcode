7479533966c671397aabb06aa60a7a34aa29c554
ana0998 <ana-gabriela.flocea@student.tuiasi.ro>
2020-11-21 13:29:58 +0200

    Actualizare componenta de vizualizare pentru a afisa toate ofertele dintr-o zona selectata

--- RoTravel/RoTravel/src/app/oferte/vizualizare-oferte/vizualizare-oferte.component.ts
+++ RoTravel/RoTravel/src/app/oferte/vizualizare-oferte/vizualizare-oferte.component.ts
@@ -10,13 +10,42 @@ export class VizualizareOferteComponent implements OnInit {
 
   constructor(private httpService: HttpClient) { }
   locatii:string [];
+  oferte: string[];
+  numeLocatie:string;
+  numeOfertaSelectata:string;
+  urlOferte:string;
+  destinatieSelectata:boolean;
+  ofertaSelectata:boolean;
 
   ngOnInit(): void {
+    this.destinatieSelectata = false;
+    this.ofertaSelectata = false;
     this.httpService.get('./assets/destinatii.json').subscribe(
       locatie =>{
         this.locatii = locatie as string [];
       },
     )
+    
+  }
+  
+  onClickDestination(numeLocatie:string):void{
+
+    this.destinatieSelectata = true;
+    this.numeLocatie = numeLocatie.split(" ").join("");
+    console.log(this.numeLocatie);
+    this.urlOferte = './assets/'+this.numeLocatie+'.json';
+    this.httpService.get(this.urlOferte).subscribe(
+      oferta =>{
+        this.oferte = oferta as string [];
+      },
+    )
   }
 
+  onClickOfert(numeofertaSelectata:string,numeLocatie:string):void{
+    this.ofertaSelectata = true;
+    this.numeLocatie = numeLocatie.split(" ").join("");
+    this.urlOferte= './assets/'+this.numeLocatie+'.json';
+    //trebuie citit din json doar oferta selectata din html.
+  }
+ 
 }
