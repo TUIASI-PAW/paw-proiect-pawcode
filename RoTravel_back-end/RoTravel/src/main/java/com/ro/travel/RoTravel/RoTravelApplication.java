446806b30c474f5425ececd384eee57a6fb697a7
MarianClaudiuScripcaru <61143625+MarianClaudiuScripcaru@users.noreply.github.com>
2020-11-30 09:01:07 +0200

    structura back-end

---
+++ RoTravel_back-end/RoTravel/src/main/java/com/ro/travel/RoTravel/RoTravelApplication.java
@@ -0,0 +1,13 @@
+package com.ro.travel.RoTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoTravelApplication.class, args);
+	}
+
+}
