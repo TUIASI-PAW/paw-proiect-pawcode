e2a04642a6b91f8e67a4a50f1c91b5cfc9006e53
Ghiba Razvan <razvan-constantin.ghiba@student.tuiasi.ro>
2020-12-01 17:48:01 +0200

    update

--- RoTravel/RoTravel/src/app/utilizatori/autentificare-utilizatori/autentificare-utilizatori.component.ts
+++ RoTravel/RoTravel/src/app/utilizatori/autentificare-utilizatori/autentificare-utilizatori.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Router} from '@angular/router';
-
+import {UtilizatoriService} from '../utilizatori.service'
+import {User} from '../models'
+import { Route } from '@angular/compiler/src/core';
 @Component({
   selector: 'app-autentificare-utilizatori',
   templateUrl: './autentificare-utilizatori.component.html',
   styleUrls: ['./autentificare-utilizatori.component.css']
 })
 export class AutentificareUtilizatoriComponent implements OnInit {
+  user =new User()
 
-  constructor() { }
+  constructor(private _service:UtilizatoriService,private _route:Router) { }
 
   ngOnInit(): void {
   }
 onSubmit(form:NgForm){
-  console.log(form.value)
-  form.reset
+
+  this._service.loginUserFromRemote(this.user).subscribe(
+  
+    data=>{console.log(data); 
+
+      if(data!=null){
+        this._route.navigate(['/oferte'])
+      }
+  
+  },
+  error=>{
+    console.log("exception occured")
+  }
+
+  )
 
 }
 
