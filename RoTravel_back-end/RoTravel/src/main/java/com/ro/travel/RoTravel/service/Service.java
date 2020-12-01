541887e69f7738c8765b00a50bbb0214e7e48d8f
Ghiba Razvan <razvan-constantin.ghiba@student.tuiasi.ro>
2020-12-01 15:25:38 +0200

    Adaugare Service

---
+++ RoTravel_back-end/RoTravel/src/main/java/com/ro/travel/RoTravel/service/Service.java
@@ -0,0 +1,21 @@
+package com.ro.travel.RoTravel.service;
+
+import com.ro.travel.RoTravel.repository.*;
+import com.ro.travel.RoTravel.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    public User saveUser(User user){
+        return userRepo.save(user);
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepo.findByEmail(email);
+    }
+}
